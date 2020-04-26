14ade4eb0ab6e3998439cc17fc151a9da015db5c
adrianoadvrio <38116466+adrianoadvrio@users.noreply.github.com>
2020-04-26 07:03:55 -0300

    26_4  7_03

--- apiRest/src/main/java/com/produtos/apiRest/repository/ProdutoRepository.java
+++ apiRest/src/main/java/com/produtos/apiRest/repository/ProdutoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apiRest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	Produto findById(long id);
 
 }
--- apiRest/src/main/java/com/produtos/apiRest/resources/ProdutoResource.java
+++ apiRest/src/main/java/com/produtos/apiRest/resources/ProdutoResource.java
@@ -1,10 +1,39 @@
 package com.produtos.apiRest.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apiRest.models.Produto;
+import com.produtos.apiRest.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping(value ="/api" )
 public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+    public List<Produto> listaProdutos(){
+		return produtoRepository.findAll();
+	}
+	
 
+	@GetMapping("/produto/{id}")
+    public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+		return produtoRepository.findById(id);
+	}
+	
+	
+	@PostMapping("/produto")
+	public Produto salvarProduto(@RequestBody Produto produto) {
+	return produtoRepository.save(produto);
+}
 }
