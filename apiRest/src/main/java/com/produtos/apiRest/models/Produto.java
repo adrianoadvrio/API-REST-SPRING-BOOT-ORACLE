91e408049a55e44b5989bfdeb56f5a9e74c747f9
adrianoadvrio <38116466+adrianoadvrio@users.noreply.github.com>
2020-04-25 22:17:38 -0300

    beta 2.2

--- apiRest/src/main/java/com/produtos/apiRest/models/Produto.java
+++ apiRest/src/main/java/com/produtos/apiRest/models/Produto.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "TB_PRODUTO")
+@Table(name = "TB_PRODUTO")///////////////////
 public class Produto implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -28,6 +28,7 @@ public class Produto implements Serializable{
 	
 	@NotNull
 	private BigDecimal valor;
+	
 	
 
 }						
